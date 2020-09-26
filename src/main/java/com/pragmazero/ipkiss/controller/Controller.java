9ce5d1debd8e752e7d10ad73030a9559a7eb7280
Thaina Mariani <marianithaina@gmail.com>
2020-09-26 11:28:39 -0300

    adding reset method

--- src/main/java/com/pragmazero/ipkiss/controller/Controller.java
+++ src/main/java/com/pragmazero/ipkiss/controller/Controller.java
@@ -3,8 +3,13 @@ package com.pragmazero.ipkiss.controller;
 //import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import com.pragmazero.ipkiss.pojo.Event;
+import com.pragmazero.ipkiss.repository.AccountRepository;
 
 //import com.pragmazero.ipkiss.repository.AccountRepository;
 
@@ -12,11 +17,16 @@ import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExcep
 @RestController
 public class Controller extends ResponseEntityExceptionHandler {
 
-	// @Autowired
-	// public AccountRepository repository;
+	//@Autowired
+	public AccountRepository repository;
 
 	@GetMapping("/")
 	public String home() {
 		return "Hi";
 	}
+	
+    @PostMapping("reset")
+    public void reset(){
+        repository = new AccountRepository();
+    }
 }
